a7b6cc4dec15f023de40dcddaff42ec4fdfc26ae
KeanY <keanymenaberrocal13@gmail.com>
2025-11-01 22:54:56 -0600

    Creacion inicial de identity para probar register y login

--- Tienda-Restaurante/Program.cs
+++ Tienda-Restaurante/Program.cs
@@ -26,10 +26,15 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
+
 
 app.Run();
