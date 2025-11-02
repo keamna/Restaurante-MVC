4c6d8be53c5ade0f415cbec30b1ac4eeab3da271
KeanY <keanymenaberrocal13@gmail.com>
2025-11-02 17:46:50 -0600

    Muestra los platillos en la orden completo.

---
+++ Tienda-Restaurante/Migrations/20251102234248_Cuarta.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tienda_Restaurante.Migrations
+{
+    /// <inheritdoc />
+    public partial class Cuarta : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Orden",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Orden",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "MobileNumber",
+                table: "Orden",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Orden",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentMethod",
+                table: "Orden",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Orden");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Orden");
+
+            migrationBuilder.DropColumn(
+                name: "MobileNumber",
+                table: "Orden");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Orden");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentMethod",
+                table: "Orden");
+        }
+    }
+}
--- Tienda-Restaurante/Models/Orden.cs
+++ Tienda-Restaurante/Models/Orden.cs
@@ -1,6 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Security.Cryptography.Pkcs;
 
 namespace Tienda_Restaurante.Models
 {
@@ -16,6 +15,26 @@ namespace Tienda_Restaurante.Models
         [Required]
         public int OrdenEstadoId { get; set; }
         public bool IsDeleted { get; set; } = false;
+
+        [Required]
+        [MaxLength(30)]
+        public string? Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Required]
+        public string? MobileNumber { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+
+        public string? Address { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string? PaymentMethod { get; set; }
 
         public OrdenEstado OrdenEstado { get; set; }
 
