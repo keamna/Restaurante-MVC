95e73b908e313cd913337497240220e31b526677
IJxCP <Jozabadchaves@gmail.com>
2025-11-05 04:15:57 -0600

    Add files via upload

---
+++ Tienda-Restaurante/Views/AdminOperations/Reserva.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Tienda_Restaurante.Views.AdminOperations
+{
+    public class ReservaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
